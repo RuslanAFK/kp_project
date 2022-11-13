877447192cb7d5ddbe4f1c3e6faf20a00dc4c2dd
Ruslan Pundak <gymgyo93@gmail.com>
2022-11-13 15:20:04 +0200

    Created queue inside CashOffice

--- src/models/CashOffice.java
+++ src/models/CashOffice.java
@@ -1,16 +1,19 @@
 package models;
 
-
+import java.util.LinkedList;
 
 public class CashOffice {
     private Position position;
+    private LinkedList<Client> queue;
     private boolean disabled = false;
     public CashOffice(Position position, boolean disabled) {
         this.disabled = disabled;
         this.position = new Position(position.x, position.y);
+        queue = new LinkedList<>();
     }
     public CashOffice(Position position) {
         this.position = new Position(position.x, position.y);
+        queue = new LinkedList<>();
     }
 
     public Position getPosition() {
@@ -20,5 +23,11 @@ public class CashOffice {
     @Override
     public String toString() {
         return "X: " + position.x + ", Y: " + position.y;
+    }
+    public void addClient(Client client) {
+        queue.add(client);
+    }
+    public void deleteClient() {
+        queue.removeFirst();
     }
 }
