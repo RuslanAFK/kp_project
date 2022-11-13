22f60526ef48a08d291e572c4145e0eaa88e94e4
karpikorop <karpikorop3@gmail.com>
2022-11-13 15:29:54 +0300

    Logging and queue files added

---
+++ src/models/LoggingItem.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.sql.Time;
+
+public class LoggingItem {
+    private int clientId;
+    private int cashOfficeId;
+    private Time startTime;
+    private Time endTime;
+}
